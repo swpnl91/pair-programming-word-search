3e0f5b00f32036bc95fd66ee8db26c76213974a8
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2021-12-02 03:23:12 +0000

    adding wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    let str = "";
+    if (letters.length === 0) {
+      return undefined;
     }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.length === 0) {
+          return undefined;
+        }
+        if (l.includes(word)) {
+            return true; 
+        }
+    }
+
+     for (let i = 0; i < horizontalJoin[0].length; i++) {
+            for (let j = 0; j < horizontalJoin.length; j++) {
+              str += horizontalJoin[j][i];
+            }
+            if (str.includes(word)) {
+              return true;
+            }
+            str = "";
+        }
+  return false; 
+};
+
 
 module.exports = wordSearch
